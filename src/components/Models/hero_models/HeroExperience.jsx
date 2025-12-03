66703ad6da049043126e1988c048d509be250f7c
Siddhesh Nimodiya <siddheshnimodiya22@gmail.com>
2025-12-03 22:14:26 +0530

    Fixed scrooling on mobile phone
    Disabled zoom on mobile

--- src/components/Models/hero_models/HeroExperience.jsx
+++ src/components/Models/hero_models/HeroExperience.jsx
@@ -12,15 +12,22 @@ const HeroExperience = () => {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}
+      style={{
+        touchAction: "pan-y",  // 🌟 allow vertical scrolling
+      }}
+    >
       {/* deep blue ambient */}
       <ambientLight intensity={0.2} color="#1a1a40" />
       {/* Configure OrbitControls to disable panning and control zoom based on device type */}
       <OrbitControls
         enablePan={false} // Prevents panning of the scene
-        enableZoom={!isTablet} // Disables zoom on tablets
-        maxDistance={20} // Maximum distance for zooming out
-        minDistance={5} // Minimum distance for zooming in
+        enableZoom={!isMobile} // Disables zoom on mobiles
+        rotateSpeed={0.8}   //smoother touch rotation 
+        touches={{
+          ONE: 1, // allow rotation with one finger
+          TWO: 0, // disable zoom/pan gestures
+        }}
         minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
         maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
       />
