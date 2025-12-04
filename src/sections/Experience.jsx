cef9c1bbaf5f6d132b38afc5cdfd33b15601344e
Siddhesh Nimodiya <siddheshnimodiya22@gmail.com>
2025-12-04 22:43:11 +0530

    fixed journey link

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -91,7 +91,7 @@ const Experience = () => {
 
   return (
     <section
-      id="experience  "
+      id="experience"
       className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
     >
       <div className="w-full h-full md:px-20 px-5">
